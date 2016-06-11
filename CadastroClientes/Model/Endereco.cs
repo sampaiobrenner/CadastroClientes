e2e1b76fb0b261a2e642367e124fa4d80afba91a
Diego Montebruni <montebrunidiego@gmail.com>
2016-06-11 10:40:39 -0300

    Envio do Projeto
    Telas Prontas.

---
+++ CadastroClientes/Model/Endereco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroClientes.Model
+{
+    public class Endereco
+    {
+        public string Descricao { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Uf { get; set; }
+        public string Cep { get; set; }
+    }
+}
